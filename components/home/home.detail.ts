906c98b94bb454ba3fefc397637ddac33c9a3a65
Fumio SAGAWA <fumio.sagawa@gmail.com>
2016-04-28 12:13:47 +0900

    Update home.detail.ts

--- components/home/home.detail.ts
+++ components/home/home.detail.ts
@@ -4,7 +4,7 @@ import {Component, Input, Output, EventEmitter} from 'angular2/core'
   selector: 'todo-detail',
   template: `
     <p class="input-group">
-      <input type="text" [(ngModel)]="data" class="form-control">
+      <input type="text" [ngModel]="data" class="form-control">
       <span class="input-group-btn">
         <button class="btn btn-danger" (click)="onClick($event)" aria-label="Remove">X</button>
       </span>
@@ -13,10 +13,13 @@ import {Component, Input, Output, EventEmitter} from 'angular2/core'
 })
 
 export class DetailApp {
-  @Input('todo-data') data: string;
-  @Output('on-delete') onDelete = new EventEmitter();
+  @Input('todo-data')
+  private data: string;
+  
+  @Output('on-delete')
+  private onDelete = new EventEmitter();
+  
   onClick($event): void {
     this.onDelete.emit($event);
   }
 }
-　
